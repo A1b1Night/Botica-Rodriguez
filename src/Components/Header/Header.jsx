6342292484fd0f75fb5f302997b42005ad495d31
A1Jhonas <carlos_55232@hotmail.com>
2023-04-02 18:24:34 -0500

    Menu denuevo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,26 +1,52 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
-
+import {AiOutlineClose} from 'react-icons/ai'
 
 const Header = () => {
-
+  const [navbar, setNavbar] = useState(false);
 
   return (
-    <div className='bg-[#5ABB98] h-20 w-full fixed'>
+    <div className='bg-[#5ABB98] h-20 w-full fixed top-0 left-0 right-0 z-10'>
 
-      <input type='checkbox' id='check' hidden />
-      <label htmlFor="check" className='float-right text-white text-3xl leading-[80px] mr-10 lg:hidden'>
-        <GiHamburgerMenu />
-      </label>
+      <div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8'>
+        <div>
+          <div className='flex items-center justify-between py-3 md:py-5 md:block'>
+            {/*LOGO*/}
+            <Link to="#">
+              <h1 className='text-2xl text-white'>BOTICA</h1>
+            </Link>
+            {/*BOTON ABRIR CERRAR*/}
+            <div className='md:hidden'>
+              <button className="p-2 text-[#262E72] rounded-md outline-none focus:border-[#262E72] focus:border" onClick={() => setNavbar(!navbar)}>
+              {navbar ? (
+                    <AiOutlineClose className='w-[30px] h-[30px]' />
+                  ) : (
+                    <GiHamburgerMenu className='focus:border-none active:border-none w-[30px] h-[30px]' />
+                  )}
+              </button>
+            </div>
+          </div>
+        </div>
 
-      <label className='text-white leading-[80px] md:leading-[80px] pl-12 md:pl-24 md:text-4xl text-3xl'>BOTICA</label>
-
-      <ul className='float-right mr-10 flex leading-[80px] space-x-4 text-white uppercase rounded fixed lg:relative h-[100vh] lg:h-0 w-[100%] lg:w-fit top-20 lg:top-0 left-[-100%] lg:left-0 transition-all duration-300 lg:transition-none text-center bg-[#5ABB98]'>
-        <Link to="#">Inicio</Link>
-        <nav className='text-[#262E72]'>|</nav>
-        <Link to="#">¿Quienes Somos?</Link>
-      </ul>
-      
+        <div>
+            <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'}`}>
+              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+                <li className=" text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-[#5B58D3]  border-[#5B58D3]  md:hover-[#5B58D3] md:hover:bg-transparent">
+                  <Link to="#" onClick={() => setNavbar(!navbar)}>
+                    Inicio
+                  </Link>
+                </li>
+                <nav className='text-xl text-[#5B58D3] py-2 md:px-6 text-center border-b-2 md:border-b-0 '>|</nav>
+                <li className="text-xl text-white py-2 px-6 text-center border-b-2 md:border-b-0  hover:bg-[#5B58D3]  border-[#5B58D3]  md:hover:text-[#5B58D3] md:hover:bg-transparent">
+                  <Link to="#" onClick={() => setNavbar(!navbar)}>
+                    ¿Quienes Somos?
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>    
+      </div>
     </div>
   )
 }
