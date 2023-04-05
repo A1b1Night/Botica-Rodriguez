57842294ed3e509f7cd5c445e1f4d0fc72fe504c
A1Jhonas <carlos_55232@hotmail.com>
2023-04-05 14:06:15 -0500

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ return (
 
 <footer class="bg-[#37306B] rounded-lg shadow m-4 dark:bg-[#37306B] w-10/12 mx-auto">
   <div class="w-full mx-auto max-w-screen-xl p-5 md:flex md:items-center md:justify-between">
-    <span class="text-xl text-white sm:text-center dark:text-white"> © 2023 <a href="https://flowbite.com/"
-        class="hover:underline">Botica™</a>. Todos los derechos reservados.
+    <span class="text-xl text-white sm:text-center dark:text-white"> © 2023 
+      <Link to="#"class="hover:underline">Botica™</Link>. Todos los derechos reservados.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-mediu  m text-white dark:text-white sm:mt-0">
       <li>
