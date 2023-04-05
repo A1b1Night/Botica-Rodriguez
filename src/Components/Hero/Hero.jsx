98f1e9f2677557fb2a281b60ca4ad8d0096076b4
A1Jhonas <carlos_55232@hotmail.com>
2023-04-05 14:50:03 -0500

    Circulos

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import Farmaceutica from '../../Assets/Farmaceutica.png'
 import Wave from '../../Assets/Vector 1.png'
 import Wave2 from '../../Assets/Vector 2.png'
 
+
 import React from 'react'
 
 const Hero = () => {
@@ -10,12 +11,14 @@ const Hero = () => {
         <img src={Wave2} alt='' className='w-full h-screen bottom-0 left-0 ml-[-15px] absolute' />
         <img src={Wave} alt='' className='w-full h-screen bottom-0 left-0 absolute' />
         <div className=' w-full h-screen items-center text-center flex justify-around text-white'>
-            <img src={Farmaceutica} alt='' className=''/>
-            <div className='z-0'>
-                <h1 className='text-7xl'>BOTICA RODRIGUEZ</h1>
-                <h3>Mas de 15 años vendiendo medicamentos a Comas</h3>
-                <h4>Tu salud es lo mas importante ❤</h4>
-            </div>
+
+          <img src={Farmaceutica} alt='' className='z-0 w'/>
+
+          <div className='z-0'>
+            <h1 className='text-7xl'>BOTICA RODRIGUEZ</h1>
+            <h3>Mas de 15 años vendiendo medicamentos a Comas</h3>
+            <h4>Tu salud es lo mas importante ❤</h4>
+          </div>
         </div>
     </div>
   )
